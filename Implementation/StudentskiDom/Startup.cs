586d44352b4e6f826093672d880a16a56f157bdf
Emir Mujanovic <emujanovic3@etf.unsa.ba>
2020-05-25 19:34:34 +0200

    Everything ready for migration

--- Implementation/StudentskiDom/Startup.cs
+++ Implementation/StudentskiDom/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StudentskiDom.Models;
 
 namespace StudentskiDom
 {
@@ -23,7 +25,11 @@ namespace StudentskiDom
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<StudentskiDomContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DbConnection")));
+
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
