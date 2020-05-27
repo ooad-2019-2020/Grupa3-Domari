31cc0088efd645062ba441992b7cf410eeb1deb3
Emir Mujanovic <emujanovic3@etf.unsa.ba>
2020-05-27 21:57:12 +0200

    ListaStudenata view added

--- Implementation/StudentskiDom/Controllers/HomeController.cs
+++ Implementation/StudentskiDom/Controllers/HomeController.cs
@@ -69,6 +69,11 @@ namespace StudentskiDom.Controllers
             return View();
         }
 
+        public IActionResult ListaStudenata()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
