0c18e5dc60d953ab8582abb7038becef3ef95174
tmehulic1 <tmehulic1@etf.unsa.ba>
2020-05-27 20:06:13 +0200

    DnevniMeni view added

--- Implementation/StudentskiDom/Controllers/HomeController.cs
+++ Implementation/StudentskiDom/Controllers/HomeController.cs
@@ -59,6 +59,11 @@ namespace StudentskiDom.Controllers
             return View();
         }
 
+        public IActionResult DnevniMeni()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
