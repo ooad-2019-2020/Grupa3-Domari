06615c3f0998b9786f6afe84793f15a739d18f09
nhastor1 <nhastor1@etf.unsa.ba>
2020-05-29 14:27:07 +0200

    Conglicts resolving

--- Implementation/StudentskiDom/Controllers/ZahtjevController.cs
+++ Implementation/StudentskiDom/Controllers/ZahtjevController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Azure.Amqp.Transaction;
 using Microsoft.EntityFrameworkCore;
 using StudentskiDom.Models;
 
